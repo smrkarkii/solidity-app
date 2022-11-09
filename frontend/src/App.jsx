10a60c8fbc18dfac1a1fd4ce063a459b185e0a67
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-11-09 21:32:06 +0545

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,35 @@ import axios from "axios";
 import { ethers } from "ethers";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum != undefined) {
+      const { ethereum } = window;
+      console.log("yes");
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      let accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(`Found account is ${accounts[0]}`);
+      console.log("connected to " + chainId);
+      setCurrentAccount(accounts[0]);
+    } else {
+      console.log("connect metamask");
+    }
+  };
+
   return (
     <div className="home">
-      <button>Connect Wallet</button>
-      <input id="bid-amount">Enter bid amount</input>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      {/* <input id="bid-amount">Enter bid amount</input>
 
       <button>Bid</button>
       <button>Withdraw</button>
       <button>End auction</button>
       <button>Benificiary</button>
       <button>Highest Bid</button>
-      <button>Highest Bidder</button>
+      <button>Highest Bidder</button> */}
     </div>
   );
 }
